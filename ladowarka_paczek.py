ab7640c46e72a8b76c5aa23438e18c851b36f8fa
Artur Mygelski <arturmygelski@gmail.com>
2024-06-01 09:18:12 +0200

    finished

--- ladowarka_paczek.py
+++ ladowarka_paczek.py
@@ -7,7 +7,7 @@ waga_obecnej_paczki = 0
 max_wykorzystanie_wagi = 0
 numer_paczki_z_max_wykorzystaniem_wagi = 1
 for przedmiot in range(ilosc_przedmiotow):
-    waga_przedmiotu = int(input("podaj ilosc wagę każdego z przedmiotów: "))
+    waga_przedmiotu = int(input(f"podaj wagę przedmiotu {przedmiot+1}: "))
     if 1 <= waga_przedmiotu <= 10:
         waga_przedmiotów += waga_przedmiotu
         if waga_przedmiotu + waga_obecnej_paczki > max_waga_paczki:
@@ -19,13 +19,19 @@ for przedmiot in range(ilosc_przedmiotow):
         else:
             waga_obecnej_paczki += waga_przedmiotu
     else:
-        print("przedmiot nie mieści się w zakresie referencyjnym 1-20")
+        print("podano nieprawidłową wagę przedmiotu")
 if max_waga_paczki - waga_obecnej_paczki > max_wykorzystanie_wagi:
     max_wykorzystanie_wagi = max_waga_paczki - waga_obecnej_paczki
     numer_paczki_z_max_wykorzystaniem_wagi = ilosc_paczek
 
-print(f"ilosc dodanych przedmiotów ogółem to: {ilosc_przedmiotow}")
-print(f"waga wszystkich przedmiotów to {waga_przedmiotów}")
-print(f"suma paczek to: {ilosc_paczek}")
-print(f"suma pustych kilogramów w paczkach to: {ilosc_paczek * max_waga_paczki - waga_przedmiotów}")
-print(f"najwiecej pustych kilogramów: {max_wykorzystanie_wagi}, w paczce nr paczki: {numer_paczki_z_max_wykorzystaniem_wagi}")
+if ilosc_paczek == 1:
+    poprawny_wyraz = "paczkę"
+elif 2 <= ilosc_paczek <= 4:
+    poprawny_wyraz = "paczki"
+else:
+    poprawny_wyraz = "paczek"
+
+print(f"wysłano {ilosc_paczek} {poprawny_wyraz} ")
+print(f"wysłano {waga_przedmiotów} kg")
+print(f"suma pustych kilogramów: {ilosc_paczek * max_waga_paczki - waga_przedmiotów} kg")
+print(f"najwiecej pustych kilogramów ma paczka: {numer_paczki_z_max_wykorzystaniem_wagi} ({max_wykorzystanie_wagi} kg)")
